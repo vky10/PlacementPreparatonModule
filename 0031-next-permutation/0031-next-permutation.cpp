140e602ca7e589b7678e6fd382f954f52a17df51
VINAY KUMAR YADAV <115296669+vky10@users.noreply.github.com>
2023-07-04 07:11:31 +0530

    Time: 0 ms (100%), Space: 12.2 MB (19.88%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -9,11 +9,12 @@ public:
                 break;
             }
         }
-        // if(ind==-1){
-        //     reverse(nums.begin(),nums.end());
-        // }
+        if(ind==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
 
-        for(int i=n-1;i>ind && ind!=-1;i--){
+        for(int i=n-1;i>ind;i--){
             if(nums[i]>nums[ind]){
                 swap(nums[i],nums[ind]);
                 break;
